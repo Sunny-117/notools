e76a4f89cf51645115c9882fca7df7c0a8b10602
Sunny-117 <zhiqiangfu6@gmail.com>
2025-02-17 15:15:45 +0800

    chore: fix pkg entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
-  target: 'node18.12',
   splitting: true,
   cjsInterop: true,
   clean: true,
   dts: true,
-  platform: 'neutral',
+  platform: 'node'
 })
